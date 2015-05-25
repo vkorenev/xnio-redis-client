95482db76fb9cfca38af5a49fb9cf2f26592a468
Vladimir Korenev <vkorenev@gmail.com>
2015-05-25 18:13:25 +0300

    Add sendNoFlush methods

--- xnio-redis-client/src/main/java/xnioredis/RedisClient.java
+++ xnio-redis-client/src/main/java/xnioredis/RedisClient.java
@@ -36,6 +36,32 @@ public abstract class RedisClient implements AutoCloseable, Flushable {
         return send(command.apply(arg1, arg2));
     }
 
+    public final <R> ListenableFuture<R> sendNoFlush(Command<R> command) {
+        return send(command, false);
+    }
+
+    public final <T, R> ListenableFuture<R> sendNoFlush(Command1<T, R> command, T arg) {
+        return sendNoFlush(command.apply(arg));
+    }
+
+    public final <T1, T2, R> ListenableFuture<R> sendNoFlush(Command2<T1, T2, R> command, T1 arg1, T2 arg2) {
+        return sendNoFlush(command.apply(arg1, arg2));
+    }
+
+    public final <T1, T2, T3, R> ListenableFuture<R> sendNoFlush(Command3<T1, T2, T3, R> command, T1 arg1, T2 arg2, T3 arg3) {
+        return sendNoFlush(command.apply(arg1, arg2, arg3));
+    }
+
+    @SafeVarargs
+    public final <E, R> ListenableFuture<R> sendNoFlush(Command1<E[], R> command, E... arg1) {
+        return sendNoFlush(command.apply(arg1));
+    }
+
+    @SafeVarargs
+    public final <T, E, R> ListenableFuture<R> sendNoFlush(Command2<T, E[], R> command, T arg1, E... arg2) {
+        return sendNoFlush(command.apply(arg1, arg2));
+    }
+
     public abstract <R> ListenableFuture<R> send(Command<R> command, boolean autoFlush);
 
     public static <R> ListenableFuture<R> send(
@@ -74,6 +100,44 @@ public abstract class RedisClient implements AutoCloseable, Flushable {
             ListenableFuture<RedisClient> clientFuture, Command2<T, E[], R> command, T arg1, E... arg2) {
         return Futures.transform(clientFuture, (AsyncFunction<RedisClient, R>) client ->
                 client.send(command, arg1, arg2));
+    }
+
+    public static <R> ListenableFuture<R> sendNoFlush(
+            ListenableFuture<RedisClient> clientFuture, Command<R> command) {
+        return Futures.transform(clientFuture, (AsyncFunction<RedisClient, R>) client ->
+                client.sendNoFlush(command));
+    }
+
+    public static <T, R> ListenableFuture<R> sendNoFlush(
+            ListenableFuture<RedisClient> clientFuture, Command1<T, R> command, T arg) {
+        return Futures.transform(clientFuture, (AsyncFunction<RedisClient, R>) client ->
+                client.sendNoFlush(command, arg));
+    }
+
+    public static <T1, T2, R> ListenableFuture<R> sendNoFlush(
+            ListenableFuture<RedisClient> clientFuture, Command2<T1, T2, R> command, T1 arg1, T2 arg2) {
+        return Futures.transform(clientFuture, (AsyncFunction<RedisClient, R>) client ->
+                client.sendNoFlush(command, arg1, arg2));
+    }
+
+    public static <T1, T2, T3, R> ListenableFuture<R> sendNoFlush(
+            ListenableFuture<RedisClient> clientFuture, Command3<T1, T2, T3, R> command, T1 arg1, T2 arg2, T3 arg3) {
+        return Futures.transform(clientFuture, (AsyncFunction<RedisClient, R>) client ->
+                client.sendNoFlush(command, arg1, arg2, arg3));
+    }
+
+    @SafeVarargs
+    public static <E, R> ListenableFuture<R> sendNoFlush(
+            ListenableFuture<RedisClient> clientFuture, Command1<E[], R> command, E... arg1) {
+        return Futures.transform(clientFuture, (AsyncFunction<RedisClient, R>) client ->
+                client.sendNoFlush(command, arg1));
+    }
+
+    @SafeVarargs
+    public static <T, E, R> ListenableFuture<R> sendNoFlush(
+            ListenableFuture<RedisClient> clientFuture, Command2<T, E[], R> command, T arg1, E... arg2) {
+        return Futures.transform(clientFuture, (AsyncFunction<RedisClient, R>) client ->
+                client.sendNoFlush(command, arg1, arg2));
     }
 
     @Override
